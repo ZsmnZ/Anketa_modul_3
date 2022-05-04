66f52585646194264e3e164b19f72bf98155e70d
Smn <smnzarubin@yandex.ru>
2022-05-04 12:29:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Anketa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Введите имя:");
+		var name = Console.ReadLine();
+		Console.WriteLine("Сколько вам лет:");
+		var age = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Введите вашу дату рождения: ");
+		var birthdate = Console.ReadLine();
+		Console.WriteLine($"Вас зовут {name} \nВаш возраст {age} \nДата рождения{birthdate}");
+		Console.ReadKey();
+	}
+}
